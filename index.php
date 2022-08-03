ebe5cce10462c6baf0824d4e06a1263ec241b87b
Sara ALshehri <107873476+s12oilwae655@users.noreply.github.com>
2022-08-03 22:51:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -19,11 +19,8 @@ if (isset($_GET['submit'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="http://localhost/phpmyadmin/index.php?route=/sql&db=sensorvalues&table=sensor_values&pos=0">
-
-    </script>
     <title>sensor values</title>
-      <link rel ="stylesheet" href="test.css">
+    <link rel ="stylesheet" href="test.css">
 </head>
 
 <body>
@@ -33,15 +30,7 @@ if (isset($_GET['submit'])){
 <input type="submit" name="submit" value="send">
 
 </form>
-<script>
-        function Movement(movement){
-            var url = 'robot_movement.php?data='+movement;
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET' , url , false);
-            xhr.send();
-        }
-    </script>
 </body>
-
+</html>
 
 </html>
